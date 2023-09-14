c1dc7f8d0832b1f50846a71a23837ccdda277932
Marci02 <nagymarci.0218@gmail.com>
2023-09-14 11:42:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CA230911
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var versenyzok = new List<Versenyzo>();
+            var sr = new StreamReader(@"..\..\..\src\fob2016.txt",
+                Encoding.UTF8);
+            while (!sr.EndOfStream)
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+            Console.WriteLine($"3. feladat: Versenyzők száma: {versenyzok.Count}");
+
+            int f4 = versenyzok.Count(v => !v.Kategoria);
+
+            Console.WriteLine($"4. feladat: Női versenyzők aránya: " +
+                $"{f4/(float)versenyzok.Count*100:0.00}%");
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA230911
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        public int[] Pontszamok { get; set; }
+
+        public int Osszpontszam 
+        {
+            get
+            {
+                Array.Sort(Pontszamok);
+                int op = 0;
+                if (Pontszamok[0] > 0) op += 10;
+                if (Pontszamok[1] > 0) op += 10;
+                for (int i = 2; i < Pontszamok.Length; i++)
+                    op += Pontszamok[i];
+
+                return op;
+            }
+        }
+
+        public Versenyzo(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+            Kategoria = v[1] == "Felnott ferfi";
+            Egyesulet = v[2] == "n.a." ? null : v[2];
+            Pontszamok = new int[8];
+            for (int i = 3; i < v.Length; i++)
+            {
+                Pontszamok[i - 3] = int.Parse(v[i]);
+            }
+
+            //for (int i = 0; i < v.Length; i++)
+            //{
+            //    Pontszamok[i] = int.Parse(v[i + 3]);
+            //}
+        }
+
+    }
+}
