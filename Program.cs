57bd9d8376eb951aced9085b7a9f7c58bd3d721a
Marci02 <nagymarci.0218@gmail.com>
2023-09-14 13:10:40 +0200

    kesz

--- Program.cs
+++ Program.cs
@@ -21,6 +21,36 @@ namespace CA230911
 
             Console.WriteLine($"4. feladat: Női versenyzők aránya: " +
                 $"{f4/(float)versenyzok.Count*100:0.00}%");
+
+            var noiBajnok = versenyzok
+                .Where(v => !v.Kategoria)
+                .OrderBy(v => v.Osszpontszam)
+                .Last();
+
+            Console.WriteLine($"6. feladat: A bajnok női versenyző");
+            Console.WriteLine($"\tNév: {noiBajnok.Nev}");
+            Console.WriteLine($"\tEgyesület: {noiBajnok.Egyesulet}");
+            Console.WriteLine($"\tÖsszpont: {noiBajnok.Osszpontszam}");
+
+            using var sw = new StreamWriter(
+                path: @"..\..\..\src\osszpontFF.txt",
+                append: false,
+                encoding: Encoding.UTF8);
+
+            foreach (var v in versenyzok)
+                if (v.Kategoria) sw.WriteLine($"{v.Nev};{v.Osszpontszam}");
+
+            var egys = versenyzok
+                .GroupBy(v => v.Egyesulet)
+                .Where(g => g.Key is not null && g.Count() > 2);
+
+            Console.WriteLine("8. feladat: Egyesület statisztika");
+
+            foreach (var e in egys)
+            {
+                Console.WriteLine($"\t{e.Key} - {e.Count()} fő");
+            }
+            
         }
     }
 }
